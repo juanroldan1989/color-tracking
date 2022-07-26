857991d2a909fe94ba2cd22146057b9449d3ec62
Juan Roldán <juanroldan1989@gmail.com>
2022-07-26 19:31:32 -0300

    `backend` folder added with Rails API Only files
    - Karafka basic config added
    - Puma and SQLite3 installed

---
+++ backend/karafka.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require ::File.expand_path('../config/environment', __FILE__)
+
+class KarafkaApp < Karafka::App
+  setup do |config|
+    config.kafka = { 'bootstrap.servers': '127.0.0.1:9092' }
+    config.client_id = 'example_app'
+    # Recreate consumers with each batch. This will allow Rails code reload to work in the
+    # development mode. Otherwise Karafka process would not be aware of code changes
+    config.consumer_persistence = !Rails.env.development?
+  end
+
+  # Comment out this part if you are not using instrumentation and/or you are not
+  # interested in logging events for certain environments. Since instrumentation
+  # notifications add extra boilerplate, if you want to achieve max performance,
+  # listen to only what you really need for given environment.
+  Karafka.monitor.subscribe(Karafka::Instrumentation::LoggerListener.new)
+  # Karafka.monitor.subscribe(Karafka::Instrumentation::ProctitleListener.new)
+
+  routes.draw do
+    # Uncomment this if you use Karafka with ActiveJob
+    # You ned to define the topic per each queue name you use
+    # active_job_topic :default
+    topic :example do
+      consumer ExampleConsumer
+    end
+  end
+end
