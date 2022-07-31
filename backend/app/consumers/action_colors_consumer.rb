1d38f05a06d0dda7072cd31f468ed2184e5fa235
Juan Roldán <juanroldan1989@gmail.com>
2022-07-30 22:20:53 -0300

    Karafka ActionColor consumer - exception handling added

--- backend/app/consumers/action_colors_consumer.rb
+++ backend/app/consumers/action_colors_consumer.rb
@@ -5,6 +5,10 @@ class ActionColorsConsumer < ApplicationConsumer
   # Consumes the messages by inserting all of them in one go into the DB
   def consume
     ActionColor.create!(messages.payloads)
+  rescue
+    # e.g.: `action_id` is nil -> `rollback transaction` error rescued
+    #        so Karafka is able to continue processing events
+    nil
   end
 
   # Run anything upon partition being revoked
