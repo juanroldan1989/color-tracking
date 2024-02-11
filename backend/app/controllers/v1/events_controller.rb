367e4444a9c11acad9be3b7cc001a80b2623fae6
Juan Roldán <juanroldan1989@gmail.com>
2024-02-11 13:26:05 +0100

    API V1 & V2 controllers - comments added

--- backend/app/controllers/v1/events_controller.rb
+++ backend/app/controllers/v1/events_controller.rb
@@ -1,10 +1,23 @@
+# API V1 Events Controller
+# Kafka as events broker
+# Consumers fetch events from Kafka topics and store them in Postgres
+# ActionCable as broadcaster
+
+# This controller handles "click" and "hover" events
+# In the frontend: events are created when a user clicks or hovers over a color
+
 module V1
   class EventsController < ApplicationController
 
+    # GET /v1/events
+    # Given "api_key" and "action", returns the amount of clicks and hovers for each color from Postgres
+    # Endpoint for "polling" frontend script
     def index
       render json: { results: results }
     end
 
+    # POST /v1/events
+    # Events are sent to Kafka for further processing
     def create
       Karafka.producer.produce_async(
         topic: topic,
@@ -15,6 +28,8 @@ module V1
         }.to_json
       )
 
+      # Events are then broadcasted to the frontend via ActionCable
+      # The frontend then updates the dashboard with the new events data
       ActionCable.server.broadcast stream, { results: results }
     end
 
--- backend/app/controllers/v2/events_controller.rb
+++ backend/app/controllers/v2/events_controller.rb
@@ -1,10 +1,23 @@
+# API V2 Events Controller
+# Kafka as events broker
+# Consumers fetch events from Kafka topics and store them in Redis
+# ActionCable as broadcaster
+
+# This controller handles "click" and "hover" events
+# In the frontend: events are created when a user clicks or hovers over a color
+
 module V2
   class EventsController < ApplicationController
 
+    # GET /v2/events
+    # Given "api_key" and "action", returns the amount of clicks and hovers for each color from Redis
+    # Endpoint for "polling" frontend script
     def index
       render json: { results: results }
     end
 
+    # POST /v2/events
+    # Events are sent to Kafka for further processing
     def create
       Karafka.producer.produce_async(
         topic: topic,
@@ -15,6 +28,8 @@ module V2
         }.to_json
       )
 
+      # Events are then broadcasted to the frontend via ActionCable
+      # The frontend then updates the dashboard with the new events data
       ActionCable.server.broadcast stream, { results: results }
     end
 
